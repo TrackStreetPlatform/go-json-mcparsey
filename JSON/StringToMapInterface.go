460127d9926185231980a13799f3961ff3cc7c14
Altieres del Sent <altieres@trackstreet.com>
2021-10-26 12:41:23 -0400

    adding function to process array

--- JSON/StringToMapInterface.go
+++ JSON/StringToMapInterface.go
@@ -19,3 +19,17 @@ func StringToMapInterface(messageBody string, typeStr string) (map[string]interf
 		return nil, errors.New(fmt.Sprint("Json not in format expected(", typeStr, ")"))
 	}
 }
+
+func StringToArrayInterface(messageBody string, typeStr string) ([]interface{}, error) {
+	var temp interface{}
+	err := json.Unmarshal([]byte(messageBody), &temp)
+	if err != nil {
+		return nil, err
+	}
+	switch tempResponse := temp.(type) {
+	case []interface{}:
+		return tempResponse, nil
+	default:
+		return nil, errors.New(fmt.Sprint("Json not in format expected(", typeStr, ")"))
+	}
+}
